eefef059e87a5de4c6ccb4bf71a5c6de3055b5bf
MaheshCheliya <Maheshsheliya@gmail.com>
2020-10-28 14:11:26 -0400

    Initial commit

---
+++ PixelCity/Controller/MapVC.swift
@@ -0,0 +1,17 @@
+//
+//  MapVC.swift
+//  PixelCity
+//
+//  Created by Mahesh on 28/10/20.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
